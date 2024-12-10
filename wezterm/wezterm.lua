cb3712c06b7c8e898b666024951002e9ce8b5018
tamada-ichikawa <102566689+tamada-ichikawa@users.noreply.github.com>
2024-12-10 17:42:35 +0900

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,62 @@
 local wezterm = require 'wezterm'
-
+local act = wezterm.action
 local config = {}
 
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
+config.leader = {
+    -- prefixを設定した
+    key = 'g',
+    mods = 'CTRL',
+    timeout_milliseconds = 1000,
+}
+
+
+config.keys = {
+    {
+    -- paneのzoomを切り替える
+        key = 'z',
+        mods = 'LEADER',
+        action = act.TogglePaneZoomState,
+    },
+    {
+    -- paneを垂直分割する
+        key = '\\',
+        mods = 'LEADER',
+        action = act.SplitHorizontal {
+            domain = 'CurrentPaneDomain'
+        },
+    },
+    {
+    -- paneを水平分割する
+        key = '-',
+        mods = 'LEADER',
+        action = act.SplitVertical {
+            domain = 'CurrentPaneDomain'
+        },
+    },
+    {
+    -- paneを移動する
+        key = 'o',
+        mods = 'CTRL',
+        action = act.ActivatePaneDirection 'Next',
+    },
+    {
+        key = '[',
+        mods = 'LEADER',
+        action = act.ActivateCopyMode,
+    },
+}
+
+-- 表示設定(時刻, バッテリー)
+wezterm.on('update-right-status', function(window, pane)
+    local date = wezterm.strftime '%Y/%m/%d(%a) %H:%M:%S'
+    for _, b in ipairs(wezterm.battery_info()) do
+        bat = wezterm.nerdfonts.md_battery .. string.format('%.0f%%', b.state_of_charge * 100)
+    end
+    window:set_right_status(wezterm.format {
+        { Text = bat .. '  ' .. date},
+    })
+end)
+
 
 -- カラースキームの設定
 config.color_scheme = 'tokyonight'  -- なんか青
